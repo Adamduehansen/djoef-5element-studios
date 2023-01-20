edc5b6999461206eaf1723baba42cc9bf1674c38
Adam Due Hansen <adamduehan@gmail.com>
2023-01-20 07:24:37 +0100

    Adds more stable tests fro EditDocument.test.tsx

--- src/components/edit/EditDocument.test.tsx
+++ src/components/edit/EditDocument.test.tsx
@@ -1,28 +1,137 @@
-import { describe, test, expect } from 'vitest';
+import { describe, test, expect, vi } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import Document from '../../contexts/Document';
 import EditDocument from './EditDocument';
 
 describe('EditorHeader', () => {
-  test('should render title of document as input field', async (): Promise<void> => {
-    // Arrange
-    render(
-      <Document.Provider
-        // @ts-ignore
-        value={{
-          title: 'any-title',
-        }}
-      >
-        <EditDocument onDownload={() => {}} />
-      </Document.Provider>
-    );
+  describe('Title input', () => {
+    test('should render title of document in input', (): void => {
+      // Arrange
+      const title = 'any-title';
+      render(
+        <Document.Provider
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          value={{
+            title: title,
+          }}
+        >
+          <EditDocument onDownload={vi.fn()} />
+        </Document.Provider>
+      );
 
-    // Act
-    const titleInput = await screen.findByRole('textbox', {
-      name: 'Titel',
+      // Act
+      const titleInput = screen.getByRole('textbox', {
+        name: 'Titel',
+      });
+
+      // Assert
+      expect(titleInput).toHaveValue(title);
     });
 
-    // Assert
-    expect(titleInput).toBeInTheDocument();
+    test('should call setTitle in document context on change', () => {
+      // Arrange
+      const setTitleMock = vi.fn();
+      render(
+        <Document.Provider
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          value={{
+            title: 'any-title',
+            setTitle: setTitleMock,
+          }}
+        >
+          <EditDocument onDownload={vi.fn()} />
+        </Document.Provider>
+      );
+
+      // Act
+      const titleInput = screen.getByRole('textbox', {
+        name: 'Titel',
+      });
+
+      fireEvent.change(titleInput, {
+        target: {
+          value: 'new-title',
+        },
+      });
+
+      // Assert
+      expect(setTitleMock).toHaveBeenCalled();
+    });
+  });
+
+  describe('Show grid checkbox', () => {
+    test('should be checked when showGrid is true', (): void => {
+      // Arrange
+      render(
+        <Document.Provider
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          value={{
+            showGrid: true,
+          }}
+        >
+          <EditDocument onDownload={vi.fn()} />
+        </Document.Provider>
+      );
+
+      // Act
+      const showGridCheckbox = screen.getByRole('checkbox', {
+        name: 'Vis gitter',
+      });
+
+      // Assert
+      expect(showGridCheckbox).toBeChecked();
+    });
+
+    test('should not be checked when showGrid is false', (): void => {
+      // Arrange
+      render(
+        <Document.Provider
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          value={{
+            showGrid: false,
+          }}
+        >
+          <EditDocument onDownload={vi.fn()} />
+        </Document.Provider>
+      );
+
+      // Act
+      const showGridCheckbox = screen.getByRole('checkbox', {
+        name: 'Vis gitter',
+      });
+
+      // Assert
+      expect(showGridCheckbox).not.toBeChecked();
+    });
+
+    test('should call setShowGrid in document context on change', () => {
+      // Arrange
+      const setShowGridMock = vi.fn();
+      render(
+        <Document.Provider
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          value={{
+            showGrid: false,
+            setShowGrid: setShowGridMock,
+          }}
+        >
+          <EditDocument onDownload={vi.fn()} />
+        </Document.Provider>
+      );
+
+      // Act
+      const showGridCheckbox = screen.getByRole('checkbox', {
+        name: 'Vis gitter',
+      });
+
+      fireEvent.click(showGridCheckbox);
+
+      expect(setShowGridMock).toHaveBeenCalled();
+    });
   });
 });
